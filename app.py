0131ccec1f349637e9ebfadb9fb64616618c311e
Трофимов Алексей Юрьевич <Trofimov_AU@College.mirea>
2020-02-18 14:10:39 +0300

    model classes added

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
-# pip install flask
-# python -m pip install flask
-# python -m pip install flask --user
-
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from model import add_user
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print(request.form['name'])
-        return 'Form was sent'
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        password_check = request.form['password_check']
+        add_user(name, email, password)
+        return redirect('/users/' + name)
     return render_template('index.html')
 
 @app.route('/users/<name>')
 def user_page(name):
     return render_template('user.html', name=name)
-
-
-# @app.route('/users/milena')
-# def user_milena():
-#     return 'Hello, Milena!'
-
 
 
 app.run(debug=True) # app.run('0.0.0.0', '3000', debug=True)
